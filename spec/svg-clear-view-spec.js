b369b17aa2d6879f591f14c00e08079b7ac55aee
VictorParraCant <victor.parracant@gmail.com>
2018-07-18 20:03:14 +0200

    Initial commit

---
+++ spec/svg-clear-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SvgClearView from '../lib/svg-clear-view';
+
+describe('SvgClearView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
